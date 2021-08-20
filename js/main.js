94c4209bce2e272942782bb0f923aef980cae002
Igor Lyakhov <igorlyahov@gmail.com>
2021-08-20 20:08:14 +1000

    день4
    Добавляет интерактивности

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ $(document).ready(function() {
     var currentFloor = 2; //текущий этаж
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
-    //наведение мышкой на этаж
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats")
+        //наведение мышкой на этаж
     $('.home-img path').on("mouseover", function() {
         $(".home-img path").removeClass('current-floor'); // удаляем активный класс у этажей
         currentFloor = $(this).attr('data-floor'); //gjkexftv значение текущего этажа
         $(".counter").text(currentFloor); // записываем значение этажа в счетчик справа
     });
+
+    $(".home-img path").on('click', toggleModal); //вызывает модальное окно по клику
+    modalCloseButton.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
+
+    //закрывает модальное окно
 
     counterUp.on("click", function() { // отслеживаем клик по кнопке вверх
         if (currentFloor < 18) { //проверяем значение этажа, не юолее 18
@@ -35,4 +44,9 @@ $(document).ready(function() {
         $(".home-img path").removeClass('current-floor');
         $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
     })
+
+    function toggleModal() {
+        modal.toggleClass('is-open');
+    }; // функция открыть-закрыть модальное окно
+
 });
